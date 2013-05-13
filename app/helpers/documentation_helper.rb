173aa8d52e1263bb7f63d8bf01afd2c9b564c606
Buck Doyle <buck.doyle@gmail.com>
2013-05-13 19:30:12 -0400

    Add message sender/receiver tags for documentation

--- app/helpers/documentation_helper.rb
+++ app/helpers/documentation_helper.rb
@@ -3,18 +3,18 @@ module DocumentationHelper
     render partial: "txts/#{name}", formats: [:text], locals: locals
   end
 
-  def txt_pair(outgoing, incoming)
-    txt_screen(outgoing) +
+  def txt_pair(outgoing, incoming, options = {})
+    txt_screen(outgoing, options[:sender]) +
       content_tag(:div, "&rarr;".html_safe, class: 'small-1 columns') +
-      txt_screen(incoming) +
+      txt_screen(incoming, options[:receiver]) +
       content_tag(:div, "", class: 'small-1 columns')
   end
 
-  def txt_screen(message)
+  def txt_screen(message, subscriber = nil)
     if message.blank?
       content_tag(:div, '&nbsp;'.html_safe, class: 'small-5 column')
     else
-      content_tag(:pre, message, class: 'txt small-5 columns')
+      content_tag(:div, content_tag(:pre, message) + (subscriber.present? ? content_tag(:div, subscriber, class: 'subscriber') : ''), class: 'txt small-5 columns')
     end
   end
 end
