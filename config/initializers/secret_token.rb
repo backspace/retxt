019035164ca7d1d970cc27746e775b6c872f14e1
Buck Doyle <buck.doyle@gmail.com>
2014-05-02 13:40:25 -0400

    Update to use secrets.yml

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ReTxt::Application.config.secret_token = '5168734ecd4eeb8e14853b889d80d94429fb987dedaf4c7a443be8a4a1ae01d1d29c00af62ecbf60662210b2138efa86ffea3a54c3e64f3ab1f5d7d006ff7a4f'
