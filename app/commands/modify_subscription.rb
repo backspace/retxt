0057765cf124068c3ee7e2d6d15f40b1833362a8
Buck Doyle <buck.doyle@gmail.com>
2014-05-09 02:30:48 -0400

    Decompose ModifySubscription#execute

--- app/commands/modify_subscription.rb
+++ app/commands/modify_subscription.rb
@@ -10,23 +10,32 @@ class ModifySubscription
   end
 
   def execute
-    if @sender.admin
-      target = FindsSubscribers.find(@arguments)
+    return reject_unless_admin unless @sender.admin
+    return reject_missing_target unless target = FindsSubscribers.find(@arguments)
+    return reject_unsubscribed_target unless @subscription = @relay.subscription_for(target)
 
-      if target
-        subscription = @relay.subscription_for(target)
+    modify_subscription
+    notify_admins
+  end
 
-        if subscription
-          @modifier.call(subscription)
-          TxtsRelayAdmins.txt_relay_admins(relay: @relay, body: @success_message, originating_txt_id: @command_context.originating_txt_id)
-        else
-          SendsTxts.send_txt(from: @relay.number, to: @sender.number, body: I18n.t('txts.unsubscribed_target', target: @arguments), originating_txt_id: @command_context.originating_txt_id)
-        end
-      else
-        SendsTxts.send_txt(from: @relay.number, to: @sender.number, body: I18n.t('txts.missing_target', target: @arguments), originating_txt_id: @command_context.originating_txt_id)
-      end
-    else
-      SendsTxts.send_txt(from: @relay.number, to: @sender.number, body: I18n.t('txts.nonadmin'), originating_txt_id: @command_context.originating_txt_id)
-    end
+  private
+  def reject_unless_admin
+    SendsTxts.send_txt(from: @relay.number, to: @sender.number, body: I18n.t('txts.nonadmin'), originating_txt_id: @command_context.originating_txt_id)
+  end
+
+  def reject_missing_target
+    SendsTxts.send_txt(from: @relay.number, to: @sender.number, body: I18n.t('txts.missing_target', target: @arguments), originating_txt_id: @command_context.originating_txt_id)
+  end
+
+  def reject_unsubscribed_target
+    SendsTxts.send_txt(from: @relay.number, to: @sender.number, body: I18n.t('txts.unsubscribed_target', target: @arguments), originating_txt_id: @command_context.originating_txt_id)
+  end
+
+  def modify_subscription
+    @modifier.call(@subscription)
+  end
+
+  def notify_admins
+    TxtsRelayAdmins.txt_relay_admins(relay: @relay, body: @success_message, originating_txt_id: @command_context.originating_txt_id)
   end
 end
