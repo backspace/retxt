418f130248e2f79848221e2f0978ac72bb75a9bc
Buck Doyle <b@chromatin.ca>
2018-03-06 17:38:26 -0600

    Change chosen link to be shared with everyone

--- app/responses/notify_meeting_response.rb
+++ app/responses/notify_meeting_response.rb
@@ -15,7 +15,13 @@ class NotifyMeetingResponse < SimpleResponse
   end
 
   def template_parameters(recipient)
+    if meeting.chosen == recipient
+      chosen_text = "When you convene"
+    else
+      chosen_text = "You are not chosen, but if the chosen person is missing"
+    end
+
     team_code_blanks = meeting.teams.map{|team| team == recipient.team ? team.code : "[sigil for #{team.addressable_name}]"}.join('')
-    {others: (meeting.teams - [recipient.team]).map(&:addressable_name).to_sentence, region: meeting.region, code: meeting.code, team_code_blanks: team_code_blanks}
+    {chosen_text: chosen_text, others: (meeting.teams - [recipient.team]).map(&:addressable_name).to_sentence, region: meeting.region, code: meeting.code, team_code_blanks: team_code_blanks}
   end
 end
